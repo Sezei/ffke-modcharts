39ad959117ccc73d7d4e75f5522a7f77c480aa53
kate <admin@sezei.me>
2023-01-26 08:24:23 +0200

    Update 9134422683.lua

--- 9134422683.lua
+++ 9134422683.lua
@@ -1,18 +1,21 @@
 -- Modchart by Sezei#3061
-return {
- 	DisableDefault = true; -- Disable the default "modchart" - This disables the default camera zooming (one-per-section).
+return { -- FNF - Mother
+	DisableDefault = true; -- Disable the default "modchart" - This disables the default camera zooming (one-per-section).
 	SongStart = function(Framework)
 		-- This function is called when the song starts.
+		print("Started Mother")
 	end;
 	OnBeat = function(Framework, Beat)
+		print("Beat: "..Beat)
 		if Beat >= 169 and Beat <= 200 then
-			ModchartSystem.CameraZoom();
+			Framework.KEMS.CameraZoom();
 		end
 	end;
 	OnSection = function(Framework, Section)
-		ModchartSystem.CameraZoom();
+		print("Section: "..Section)
+		Framework.KEMS.CameraZoom();
 	end;
 	Variables = {
 		["HelloWorld"] = 0; -- Framework:GetKEValue("HelloWorld") => 0
 	}; -- Sets KE variables here; Accessible by doing Framework:GetKEValue("VariableName");
-}
+};
